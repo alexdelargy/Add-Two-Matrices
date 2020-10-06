df8ec3f5d9ff94bffefc9f7b4c3deb5dcf80911f
Alex Delargy <addelargy97@gmail.com>
2020-10-06 08:32:45 +0000

    created two matrices

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+x=[[] for i in range(10)]
+y=[[] for i in range(10)]
+for i in range(10):
+  for k in range(10):
+    x[i].append(random.randint(0,50))
+    y[i].append(random.randint(0,50))
+
+multiplied=[[]]
+
+
